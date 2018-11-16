12a0a8fd87aa62be841f1c8694c7e8b8387e2e1e
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-16 09:06:07 -0800

    Add boilerplate template to production and test code files

---
+++ WordCounter.Tests/ModelTests/WordCounter.Tests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ToDoList.Models;
+
+namespace WordCounter.Tests
+{
+  [TestClass]
+  public class WordFrequencyCounter
+  {
+
+    // Test methods will go here.
+
+  }
+}
---
+++ WordCounter/Model/WordCounter.cs
@@ -0,0 +1,9 @@
+namespace WordCounter.Models
+{
+  public class Item
+  {
+
+    // properties, methods, etc. will go here.
+
+  }
+}
