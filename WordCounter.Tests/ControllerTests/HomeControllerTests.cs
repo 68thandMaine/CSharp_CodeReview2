bc2c557ae648f2431ca5f634be141a6377ecb7a6
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-30 10:03:56 -0800

    Add a failing test to check that the index homecontroller can return the correct type of data from the model. In this case we return a 0 whcih is not a string so the test fails

--- WordCounter.Tests/ControllerTests/HomeControllerTests.cs
+++ WordCounter.Tests/ControllerTests/HomeControllerTests.cs
@@ -10,8 +10,7 @@ namespace WordCounter.tests
   public class HomeControllerTest
   {
     [TestMethod]
-    public void
-    Index_ReturnsCorrectView_True()
+    public void Index_ReturnsCorrectView_True()
     {
       //Arrange
       HomeController controller = new HomeController();
@@ -24,7 +23,21 @@ namespace WordCounter.tests
       //Asert
       Assert.IsInstanceOfType(indexView, typeof(ViewResult));
       //This will assert that the result of index view will be of the same datatype of a ViewResult.
+    }
 
+    [TestMethod]
+    public void Index_HasCorrectModelType_String()
+    {
+      //Arrange
+      ViewResult indexView = new HomeController().Index() as ViewResult;
+      // This will create a new ViewResult called indexView. We are then taking the View of the HomeControllers Index page and casting it as a ViewResult so that we can put data onto the rendered view to the response for compairson.
+
+      //Act
+      var result = indexView.ViewData.Model;
+      // This creates a vairable named result. By naming it var we allow c# to implicitely decide what type of data result holds. In this case we know that we are holding a rendered view response that has the datatype of the Model in it. I believe this will be a string.
+
+      //Assert
+      Assert.IsInstanceOfType(result, typeof(string));
     }
   }
 }
