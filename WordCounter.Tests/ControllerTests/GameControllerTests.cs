bbe35d9023b51d8b2a292b0acf3a74e92af7294b
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-30 10:52:16 -0800

    Add a failing test to WordCounter.Test/Controllers/GameControllerTest.cs because we expect the create method in the GameController file to return a RedirectToActionResult, but instead it returns a new EmptyResult()

--- WordCounter.Tests/ControllerTests/GameControllerTests.cs
+++ WordCounter.Tests/ControllerTests/GameControllerTests.cs
@@ -10,7 +10,20 @@ namespace WordCounter.Tests
   [TestClass]
   public class GameControllerTest
   {
-    [TestMethod]
-    public void 
+      [TestMethod]
+      public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+      {
+        //Arrange
+        GameController controller = new GameController();
+        // This will instantiate a new instance of a Game controller that we call Controller.
+
+        //Act
+        var view = controller.Create("cars", "cars drive down the road");
+        //Because the first page of the app takes two arguments in and returns them as a type of ActionResult, we are calling this variable view.
+
+        //Assert
+        Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+        //Here we assert that the variable view holds the data type RedirectToActionResult.
+      }
+    }
   }
-}
