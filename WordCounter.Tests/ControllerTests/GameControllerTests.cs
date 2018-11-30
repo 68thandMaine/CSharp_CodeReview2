8863399d2b741c47d82ed6e75ae2514256c41dd8
Chris Rudnicky <chrisrudnicky@gmail.com>
2018-11-30 12:12:03 -0800

    add test to fail the HttpGet request because the response does not return a correct type

--- WordCounter.Tests/ControllerTests/GameControllerTests.cs
+++ WordCounter.Tests/ControllerTests/GameControllerTests.cs
@@ -10,20 +10,52 @@ namespace WordCounter.Tests
   [TestClass]
   public class GameControllerTest
   {
-      [TestMethod]
-      public void Create_ReturnsCorrectActionType_RedirectToActionResult()
-      {
-        //Arrange
-        GameController controller = new GameController();
-        // This will instantiate a new instance of a Game controller that we call Controller.
+    [TestMethod]
+    public void Index_ReturnsCorrectView_True()
+    {
+      //Arrange
+      GameController controller = new GameController();
 
-        //Act
-        var view = controller.Create("cars", "cars drive down the road");
-        //Because the first page of the app takes two arguments in and returns them as a type of ActionResult, we are calling this variable view.
+      //Act
+      ActionResult indexView = controller.Index();
 
-        //Assert
-        Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
-        //Here we assert that the variable view holds the data type RedirectToActionResult.
-      }
+      //Assert
+      Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    }
+
+
+
+
+    [TestMethod]
+    public void Create_ReturnsCorrectActionType_RedirectToActionResult()
+    {
+      //Arrange
+      GameController controller = new GameController();
+      // This will instantiate a new instance of a Game controller that we call Controller.
+
+      //Act
+      var view = controller.Create("cars", "cars drive down the road");
+      //Because the first page of the app takes two arguments in and returns them as a type of ActionResult, we are calling this variable view.
+
+      //Assert
+      Assert.IsInstanceOfType(view, typeof(RedirectToActionResult));
+      //Here we assert that the variable view holds the data type RedirectToActionResult.
+    }
+    [TestMethod]
+    public void Create_RedirectsToCorrectAction_Index()
+    {
+      //Arrange
+      GameController controller = new GameController();
+      RedirectToActionResult actionResult = controller.Create("chairs","Look at all these chairs here! I've never sat in so many plush chairs") as RedirectToActionResult;
+      //This will create a new GameController called controller. I then call the create Create() method on controller and pass in two strings and then cast controller from a GameController type to a RedirectToActionResult type.
+
+      //Act
+      string result = actionResult.ActionName;
+      //This will store the value that is passed into the RedirectToAction method that Create returns. It is passed a string as a paramater so result is set to a string.
+
+      //Assert
+      Assert.AreEqual(result, "Index");
+      //I assert that the string that the create method returns is equal to the string "index"
     }
   }
+}
--- WordCounter/Controllers/GameController.cs
+++ WordCounter/Controllers/GameController.cs
@@ -1,16 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using WordCounter.Models;
-using System.Collections.Generic;
+
 
 namespace WordCounter.Controllers
 {
-  public class GameController : Controllers
+  public class GameController : Controller
   {
-    [HttpPost("/game")]
+    [HttpGet("/game")]
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+
+    [HttpPost("/game/new")]
     public ActionResult Create(string word, string sentence)
     {
+      //TO FAIL---------------
+      //To fail this test returning correct action type we need to return a type of ActionResult that is not a RedirectToActionResult
+      // WordFrequencyCounter newWordFrequencyCounter = new WordFrequencyCounter(word, sentence);
+
+      // return new EmptyResult();
+
+      //TO PASS----------------
       WordFrequencyCounter newWordFrequencyCounter = new WordFrequencyCounter(word, sentence);
-      return RedirectToAction("index")
+      return RedirectToAction("Index");
     }
   }
 }
